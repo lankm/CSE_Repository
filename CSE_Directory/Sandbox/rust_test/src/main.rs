c858dda49a0bc3f88648be5709c622c828f209a7
Landon Moon <lankm@live.com>
2023-08-22 12:55:23 -0500

    Rust testing

--- CSE_Directory/Sandbox/rust_test/src/main.rs
+++ CSE_Directory/Sandbox/rust_test/src/main.rs
@@ -1,3 +1,25 @@
+#[allow(dead_code)]
+struct Coordinates {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+#[allow(dead_code)]
+fn struct_ex() {
+    let _my_coords = Coordinates {
+        x:0,
+        y:0,
+        z:0,
+    };
+}
+
+fn function(x: &i32) {
+    println!("{x}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 5;
+    function(&x);
+    x += 1;
+    println!("{x}");
 }
--- CSE_Directory/Sandbox/rust_test/target/debug/deps/rust_test.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lankm\Documents\Github_Repositories\UTA_Repository\CSE_Directory\Sandbox\rust_test\target\debug\deps\rust_test.exe: src\main.rs
-
-C:\Users\lankm\Documents\Github_Repositories\UTA_Repository\CSE_Directory\Sandbox\rust_test\target\debug\deps\rust_test.d: src\main.rs
-
-src\main.rs:
