bf8579137d30c1ec25691ca825cae664f7235b65
Landon Moon <lankm@live.com>
2023-09-09 00:03:19 -0500

    Did some work on ML HW

---
+++ CSE_Directory/CSE_Assignments/CSE_4309-ML/Assignment2/naive_bayes.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def naive_bayes(training_file, test_file):
+  # training
+
+
+
+
+
+  # training...
+  # foreach class-attribute pair - print("Class %d, attribute %d, mean = %.2f, std = %.2f" % ())
+
+  # classification...
+  # foreach test file line - print("ID=%5d, predicted=%3d, probability = %.4f, true=%3d, accuracy=%4.2f\n" % ())
+  # print("classification accuracy=%6.4f" % ())
+  return 0
