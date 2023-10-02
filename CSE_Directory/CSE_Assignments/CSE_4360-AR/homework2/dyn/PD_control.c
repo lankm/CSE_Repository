c0e012ae039e61d275e40948dad54ce8294bf15f
Landon Moon <lankm@live.com>
2023-10-02 11:23:50 -0500

    start to robotics HW

---
+++ CSE_Directory/CSE_Assignments/CSE_4360-AR/homework2/dyn/PD_control.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int UTA_ID = 1001906270;
+
+double PD_control(double theta, double theta_dot, double theta_ref, double theta_dot_ref)
+{
+  return(theta_ref);
+}
