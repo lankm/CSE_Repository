5e803465a538e04975af010eab3ab04eb736cd4d
Landon Moon <lankm@live.com>
2023-10-05 16:39:36 -0500

    finished robotics

--- CSE_Directory/CSE_Assignments/CSE_4360-AR/homework2/dyn/PD_control.c
+++ CSE_Directory/CSE_Assignments/CSE_4360-AR/homework2/dyn/PD_control.c
@@ -1,26 +1,55 @@
 #include <stdio.h>
 #include <math.h>
 
-// TODO: Actually learn this and do it myself
+#define PI 3.1415926535897932384626
 
-#define G -4.1642796
-#define B -0.170857
-#define M 11.34
+#define G 4.16427960
+#define b 0.17080023
+#define I 0.08825050
 
 int UTA_ID = 1001906270;
 
+// testing variables
 double summation = 0.0;
-int count = 0;
-
+double count = 0;
 double last = 0;
 
 
 double PD_control(double theta, double theta_dot, double theta_ref, double theta_dot_ref)
 {
-  double gravity = G*cos(theta);
-  double friction = B*theta_dot;
-  double K = 3.0; // arbetrary
-  double KP = 2*sqrt(K);
+  // // torque = I*theta_dot_dot + B*theta_dot + G*cos(theta)
+
+  // // testing for G
+  // double torque = 5*theta_ref;
+  // printf("G = %.8f\n", torque/cos(theta));
+  // return torque;
+
+  // // testing for B
+  // double torque = theta_ref + G*cos(theta);
+  // if (theta_ref > 3.0) {
+  //   summation += theta_ref/theta_dot;
+  //   count += 1;
+  //   summation /= 1.0001;
+  //   count /= 1.0001;
+  // }
+  // printf("B = %.8f\n", summation/count);
+  // return torque;
+
+  // // testing for M
+  // double torque = B*theta_dot;
+  // double accel = 500*(theta_dot - last);
+  // last = theta_dot;
+  // // getting calcualted acceleration only when torque is large
+  // if (.01 > fmod(theta, PI) && -.01 < fmod(theta, PI)) {
+  //   summation += -G*cos(theta)/accel;
+  //   count += 1;
+  //   printf("I = %.8f\n", summation/count);
+  // }
+  // return torque;
   
-  return(K*M*(theta_ref-theta) - gravity - friction - KP*theta_dot);
+  double gravity = G*cos(theta);
+  double K = 100.0; // arbetrary. change as desired
+  double B = 2*sqrt(K);
+  //         spring force                  friction 
+  return I*(K*(theta_ref - theta) + (B+b)*(theta_dot_ref - theta_dot)) + gravity;
 }
