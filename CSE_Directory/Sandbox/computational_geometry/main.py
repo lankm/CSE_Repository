e6b6c7e5817ac07140f56c183fdcfe1d4707ae82
Landon Moon <lankm@live.com>
2023-11-28 13:39:07 -0600

    The usual update

--- CSE_Directory/Sandbox/computational_geometry/main.py
+++ CSE_Directory/Sandbox/computational_geometry/main.py
@@ -1,38 +1,9 @@
-import pygame
 import sys
+from linalg import *
 
 def main():
-    pygame.init()
-
-    # Set up display
-    width, height = 800, 600
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Simulation Example")
-
-    # Simulation variables
-    simulation_objects = [{'x': 50, 'y': 50, 'radius': 20, 'color': (255, 0, 0)}]
-
-    # Main simulation loop
-    clock = pygame.time.Clock()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        # Update simulation logic here
-
-        # Draw simulation objects
-        screen.fill((255, 255, 255))  # White background
-        for obj in simulation_objects:
-            pygame.draw.circle(screen, obj['color'], (obj['x'], obj['y']), obj['radius'])
-            obj['x'] += 1
-
-        pygame.display.flip()
-
-        # Cap the frame rate
-        clock.tick(60)
-
+    pass
+    v = Matrix()
 
 if __name__ == '__main__':
     main()
