b2202f846c056777247d468758f4f0a1f8011f68
Landon Moon <lankm@live.com>
2024-01-19 22:56:01 -0600

    some GR work

--- CSE_Directory/Sandbox/test.py
+++ CSE_Directory/Sandbox/test.py
@@ -1,5 +1,24 @@
-l = [a for a in range(20)]
-print(l)
+import sympy
+from sympy import symbols, sin, cos, sinh, cosh, tanh
+from einsteinpy.symbolic import RicciTensor, RicciScalar, ChristoffelSymbols
+from einsteinpy.symbolic.metric import MetricTensor
 
-print(l[:-1])
-print(l[-1])
+syms = sympy.symbols("t r theta phi R")
+t, r, th, ph, R = syms
+a = sympy.Function('a')(t)
+metric = MetricTensor([
+    [R**2,0,0,0],
+    [0,R**2 * cosh(t)**2,0,0],
+    [0,0,R**2 * cosh(t)**2 * sin(r)**2,0],
+    [0,0,0,R**2 * cosh(t)**2 * sin(r)**2 * sin(th)**2]
+], syms[:4])
+
+# cs = ChristoffelSymbols.from_metric(metric)
+
+rt = RicciTensor.from_metric(metric)
+for row in rt.tensor():
+    print(row)
+
+print()
+rs = RicciScalar.from_metric(metric)
+print(rs.tensor())
