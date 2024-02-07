42aa035daffd6fedd69407ec2d37c3a10b6111e6
Landon Moon <lankm@live.com>
2024-02-07 10:22:26 -0600

    Vision assignment 2 setup

---
+++ CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment2/stitch_images.py
@@ -0,0 +1,21 @@
+# https://en.wikipedia.org/wiki/Random_sample_consensus
+
+import numpy as np
+
+def compute_projective_transform():
+    pass
+
+def compute_affine_transform():
+    pass
+
+def ransac():
+    pass
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
