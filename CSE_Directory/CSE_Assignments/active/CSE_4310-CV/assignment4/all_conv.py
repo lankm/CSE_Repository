6fc54f9b480d7d034b2bad2340ca958a0b3b39bf
Landon Moon <lankm@live.com>
2024-04-08 03:16:23 -0500

    had to be in a main function. idk why

--- CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment4/all_conv.py
+++ CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment4/all_conv.py
@@ -3,6 +3,10 @@ from torch import nn
 import torch.nn.functional as F
 import lightning as L
 import torchmetrics
+from torchvision import transforms
+from torchvision.datasets import Imagenette
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 
 class BaselineModel(L.LightningModule):
@@ -58,69 +62,61 @@ class BaselineModel(L.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
     
-from torchvision import transforms
-from torchvision.datasets import Imagenette
-from lightning.pytorch.callbacks.early_stopping import EarlyStopping
-from lightning.pytorch.callbacks import ModelCheckpoint
-
-
-# Prepare the dataset
-train_transforms = transforms.Compose([
-    transforms.CenterCrop(160),
-    transforms.Resize(64),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-    transforms.Grayscale()
-])
-
-test_transforms = transforms.Compose([
-    transforms.CenterCrop(160),
-    transforms.Resize(64),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-    transforms.Grayscale()
-])
-
-train_dataset = Imagenette("data/imagenette/train/", split="train", size="160px", download=False, transform=train_transforms)
-
-# Use 10% of the training set for validation
-train_set_size = int(len(train_dataset) * 0.9)
-val_set_size = len(train_dataset) - train_set_size
-
-seed = torch.Generator().manual_seed(42)
-train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_set_size, val_set_size], generator=seed)
-val_dataset.dataset.transform = test_transforms
-
-# Use DataLoader to load the dataset
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, num_workers=8, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, num_workers=8, shuffle=False)
-
-# Configure the test dataset
-test_dataset = Imagenette("data/imagenette/test/", split="val", size="160px", download=False, transform=test_transforms)
-
-model = BaselineModel()
-
-# Add EarlyStopping
-early_stop_callback = EarlyStopping(monitor="val_loss",
-                                    mode="min",
-                                    patience=5)
-
-# Configure Checkpoints
-checkpoint_callback = ModelCheckpoint(
-    monitor="val_loss",
-    mode="min"
-)
-
-# Fit the model
-trainer = L.Trainer(callbacks=[early_stop_callback, checkpoint_callback])
-trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
-
-# Evaluate the model on the test set
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=256, num_workers=8, shuffle=False)
-trainer.test(model=model, dataloaders=test_loader)
-
 def main():
-    pass
+    # Prepare the dataset
+    train_transforms = transforms.Compose([
+        transforms.CenterCrop(160),
+        transforms.Resize(64),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        transforms.Grayscale()
+    ])
 
-if __name__ == '__main__':
+    test_transforms = transforms.Compose([
+        transforms.CenterCrop(160),
+        transforms.Resize(64),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        transforms.Grayscale()
+    ])
+
+    train_dataset = Imagenette("data/imagenette/train/", split="train", size="160px", download=False, transform=train_transforms)
+
+    # Use 10% of the training set for validation
+    train_set_size = int(len(train_dataset) * 0.9)
+    val_set_size = len(train_dataset) - train_set_size
+
+    seed = torch.Generator().manual_seed(42)
+    train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_set_size, val_set_size], generator=seed)
+    val_dataset.dataset.transform = test_transforms
+
+    # Use DataLoader to load the dataset
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, num_workers=8, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, num_workers=8, shuffle=False)
+
+    # Configure the test dataset
+    test_dataset = Imagenette("data/imagenette/test/", split="val", size="160px", download=False, transform=test_transforms)
+
+    model = BaselineModel()
+
+    # Add EarlyStopping
+    early_stop_callback = EarlyStopping(monitor="val_loss",
+                                        mode="min",
+                                        patience=5)
+
+    # Configure Checkpoints
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        mode="min"
+    )
+
+    # Fit the model
+    trainer = L.Trainer(callbacks=[early_stop_callback, checkpoint_callback])
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+    # Evaluate the model on the test set
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=256, num_workers=8, shuffle=False)
+    trainer.test(model=model, dataloaders=test_loader)
+
+if __name__ == "__main__":
     main()
