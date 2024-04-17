bfc69682616c784c966196dc95c4e5abb7afc4e6
Landon Moon <lankm@live.com>
2024-04-17 15:32:47 -0500

    vision assignment

--- CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment4/all_conv.py
+++ CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment4/all_conv.py
@@ -9,26 +9,42 @@ from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks import ModelCheckpoint
 
 
-class BaselineModel(L.LightningModule):
+class AllConv(L.LightningModule):
     def __init__(self, num_classes=10):
         super().__init__()
 
-        self.estimator = nn.Sequential(
-            nn.Linear(64 * 64, 1024),
+        self.features = nn.Sequential(
+            nn.Conv2d(3,512,5, padding=2),
             nn.ReLU(),
-            nn.Linear(1024, 512),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(512,256,5, padding=2),
             nn.ReLU(),
-            nn.Linear(512, 128),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256,128,5, padding=2),
             nn.ReLU(),
-            nn.Linear(128, num_classes)
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128,64,5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64,32,5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
         )
+
+        self.classifier = nn.Linear(2 * 2 * 32, num_classes)
 
         self.accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes)
 
     def forward(self, x):
-        x = x.view(x.shape[0], -1)
+        # x is a torch tensor with dims ({batch size}, {channels}, {img length}, {img width})
+        x = self.features(x) # changes {channels} based on output channels. changes {img length}/{img width} based on padding/kernal size
+        x = x.view(x.shape[0], -1) # flatten output channels and image
 
-        return self.estimator(x)
+        return self.classifier(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -69,7 +85,6 @@ def main():
         transforms.Resize(64),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-        transforms.Grayscale()
     ])
 
     test_transforms = transforms.Compose([
@@ -77,15 +92,16 @@ def main():
         transforms.Resize(64),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-        transforms.Grayscale()
     ])
 
+    # Configure datasets
     train_dataset = Imagenette("data/imagenette/train/", split="train", size="160px", download=False, transform=train_transforms)
+    test_dataset = Imagenette("data/imagenette/test/", split="val", size="160px", download=False, transform=test_transforms)
 
     # Use 10% of the training set for validation
     train_set_size = int(len(train_dataset) * 0.9)
     val_set_size = len(train_dataset) - train_set_size
-
+    # split training dataset into training and validation sets
     seed = torch.Generator().manual_seed(42)
     train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_set_size, val_set_size], generator=seed)
     val_dataset.dataset.transform = test_transforms
@@ -93,30 +109,25 @@ def main():
     # Use DataLoader to load the dataset
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, num_workers=8, shuffle=True, persistent_workers=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, num_workers=8, shuffle=False, persistent_workers=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=256, num_workers=8, shuffle=False, persistent_workers=True)
 
-    # Configure the test dataset
-    test_dataset = Imagenette("data/imagenette/test/", split="val", size="160px", download=False, transform=test_transforms)
-
-    model = BaselineModel()
+    
 
     # Add EarlyStopping
-    early_stop_callback = EarlyStopping(monitor="val_loss",
-                                        mode="min",
-                                        patience=5)
-
+    early_stop_callback = EarlyStopping(monitor="val_loss", mode="min", patience=3)
     # Configure Checkpoints
-    checkpoint_callback = ModelCheckpoint(
-        monitor="val_loss",
-        mode="min"
-    )
+    checkpoint_callback = ModelCheckpoint( monitor="val_loss", mode="min")
+
+    model = AllConv()
 
     # Fit the model
     trainer = L.Trainer(callbacks=[early_stop_callback, checkpoint_callback], max_epochs=-1)
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     # Evaluate the model on the test set
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=256, num_workers=8, shuffle=False, persistent_workers=True)
     trainer.test(model=model, dataloaders=test_loader)
+
+    torch.save(model.state_dict(), 'model_weights/all_conv')
 
 if __name__ == "__main__":
     main()
--- CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment4/report.md
+++ CSE_Directory/CSE_Assignments/active/CSE_4310-CV/assignment4/report.md
@@ -1,10 +1,73 @@
 # Assignment 4 Report
 
+saving and loading of model weights should be done as such:
+- Save:
+torch.save(model.state_dict(), PATH)
+- Load:
+model = Model()
+model.load_state_dict(torch.load(PATH))
+
+model weights are saves as file in /model_weights/
+
+I probably could have reduced code duplication between files but senioritis is kicking is so sorry.
+
+
 ## Basic CNN
+
+implemented in basic_cnn.py
+
+I initally messed around with emulating the VGG-19 model but got abismally bad results. I belive it has something to do with the difference in image sizes and number of output classes. Additially, most of my own more complex ideas also gave similarly bad results (~10% acc, same as random guess). I implemented a decent all_conv first and decided to copy the same idea I had for the convolutional section.
+
+As stated above, I messed around with a couple more complex models but didn't get good results. I eventually landed on a more simple model that follows the idea of the all_conv code (look below). The convolutional section starts with many channels and slowly reduces the number of channels until the fully connected NN can make some sense of it. The fully connected NN I implemented is again fairly basic. It contains two hidden layers with 512 and 128 nodes respectively.
+
+- Training Loss:   0.8066
+- Validation Loss: 1.317
+- Test accuracy:   0.4525
 
 ## All Convolutional Net
 
+Implemented in all_conv.py
+
+I made a simple archetecture of my own. The idea is to combine convolutions that represent small/basic components into larger/more meaningfull components. The model starts out with a large number of channels that can represent a large amount of basic features, then reduces the channels to increase the meaningfulness of each channel. The initial convolutions might be able to reconize common edges/corners in a type of image, then later covolutions (after pooling) can reconize these basic attributes and possibly group them together to form an idea of object types.
+
+The archetecture starts with a convolution step with 512 channels and max pools it; then 256, 128, 64, 32. It does this until 32. The final result is an image that is 2x2 with 32 channels. To convert this to the given number of classes, the model does a simple estimator with no hidden layers. It is comprized of one linear transformation from (2*2*32) to the given number of classes
+
+- Training Loss:   0.6696
+- Validation Loss: 1.276
+- Test accuracy:   0.6431
+
 ## Regularization
+
+Implemented in all_conv_reg.py
+
+For regularization I implemented both dropout and data augmentation. In terms of final test accuracy, there is a slight improvement but not by much. The largest difference is that the model with dropout and trained on a data-augmented dataset took longer to start overfiting. The valication loss took longer to start increasing and started increasing at a lower rate. The final validation loss was also lower for the regularized model due regularization slowing down overfitting.
+
+![alt text](reg_train_loss.png)
+![alt text](reg_val_loss.png)
+
+- Training Loss:   0.8444
+- Validation Loss: 1.052
+- Test accuracy:   0.6721
 
 ## Transfer Learning
 
+Implemented in all_conv_trans.py
+
+from scratch comparison is in all_conv_reg_cifar10.py
+
+I chose to use the regularized all convolution model. The model starts with many convolutional channels to 'understand' basic features and slowly reduces the number of channels and img size to group the basic features into larger features. Additionally regularization in the form of dropout and data augmentation is done to reduce overfitting.
+
+While the accuracy of the model with the transfered features performed worse than the model that was trained from scratch, it trained much faster. The from-scratch model took ~4 hours to finish while the transfer learned model took ~15 minutes. This is due to only needing to train a single classifier instead of a classifier and many layers of a convolutional net.
+
+![alt text](transfer_compare_train.png)
+![alt text](transfer_compare_val.png)
+
+### from-scratch comparison
+- Training Loss:   0.6466
+- Validation Loss: 0.9955
+- Test accuracy:   0.8471
+
+### tranfer learning
+- Training Loss:   1.882
+- Validation Loss: 1.762
+- Test accuracy:   0.3762
